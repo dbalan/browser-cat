97bdb667fd0ee5e95d5ae16ed45091747df3f6fa
Dhananjay Balan <mail@dbalan.in>
2018-09-22 16:13:39 +0300

    Intial commit

---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+setup(
+    name="browser-cat",
+    version="0.1",
+    scripts=['bcat'],
+
+    # metadata to display on PyPI
+    author="Dhananjay Balan",
+    author_email="mail@dbalan.in",
+    description="Cat to browser",
+    license="BSD",
+    keywords="mutt browser cat",
+    url="http://github.com/dbalan/browser-cat",   # project home page, if any
+    project_urls={
+        "Bug Tracker": "http://github.com/dbalan/browser-cat/issues",
+        "Documentation": "http://github.com/dbalan/browser-cat",
+        "Source Code": "http://github.com/dbalan/browser-cat",
+    }
+
+    # could also include long_description, download_url, classifiers, etc.
+)
